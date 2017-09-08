a8ae42f7fbbefeb75f4da1375a9f56f3324fe305
github2136 <github2136@163.com>
2017-09-08 17:53:43 +0800

    基本功能提交

---
+++ app/src/main/java/com/github2136/selectimage/MainActivity.java
@@ -0,0 +1,30 @@
+package com.github2136.selectimage;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.github2136.selectimamge.SelectImageActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnImg = (Button) findViewById(R.id.im_select_img);
+        btnImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(MainActivity.this, SelectImageActivity.class), 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
